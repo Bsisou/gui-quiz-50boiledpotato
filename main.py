38b2f517a97bed542723ba538f12243570e090db
50boiledpotato <161266450+50boiledpotato@users.noreply.github.com>
2024-05-14 00:55:29 +0000

    starting window

--- main.py
+++ main.py
@@ -1,12 +1,42 @@
-import tkinter as tk
+from tkinter import *
+from PIL import Image, ImageTk
 
-window = tk.Tk()
-window.title("Hello world")
-window.geometry("300x300")
 
-hello = tk.Label(text="Hello world!")
-hello.pack()
-button = tk.Button(text="Click me!")
-button.pack()
+class QuizStarter:
+    def __init__(self, parent):
+        
+        #background_color = "OldLace"
+        background_color = "red"
 
-tk.mainloop()
+        
+
+        self.bg_image = Image.open("quizPageOne.png") #need to use Image if need to resize 
+        
+
+        width = 960
+        height = 540
+        self.resize_image = self.bg_image.resize((width, height))
+ 
+        self.bg_image = ImageTk.PhotoImage(self.resize_image) 
+
+
+        #frame set up
+        self.quiz_frame = Frame(parent, bg=background_color)
+        self.quiz_frame.pack()
+
+        #label for image
+        self.image_label= Label(self.quiz_frame, image=self.bg_image)
+        #
+        self.image_label.pack()
+
+
+
+
+
+#***********Starting point of the program***********#
+if __name__ == '__main__':
+    root = Tk()
+    root.geometry("960x540")
+    root.title("General Knowledge Quiz")
+    quizstarter_object = QuizStarter(root)   
+    root.mainloop() #So window won't disappear 
